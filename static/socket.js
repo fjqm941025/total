e2312fec918349cd1a2f4f6f9e00799ed9306c4d
fjqm941025 <fjqm941025@naver.com>
2020-11-02 22:48:35 +0900

    Red light update

--- static/socket.js
+++ static/socket.js
@@ -1,4 +1,11 @@
 const socket = io();
+const input_arr = ["11", "12", "13", "14", "21", "22", "23", "24"];
 socket.on('chat message', (green_light) => {
    $(`input[name=${green_light}]`).val("GREEN");
+
+    input_arr.forEach(function(input) {
+        if(green_light != input){
+            $(`input[name=${input}]`).val("RED");
+        }
+    });
  });
