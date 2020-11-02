05004b2132a30094c6ea8aecd57a9dd0607f7c21
fjqm941025 <fjqm941025@naver.com>
2020-11-03 01:55:56 +0900

    color

--- static/socket.js
+++ static/socket.js
@@ -2,10 +2,13 @@ const socket = io();
 const input_arr = ["11", "12", "13", "14", "21", "22", "23", "24"];
 socket.on('chat message', (green_light) => {
    $(`input[name=${green_light}]`).val("GREEN");
-
+   $(`input[name=${green_light}]`).css('background-color', 'green');
     input_arr.forEach(function(input) {
         if(green_light != input){
             $(`input[name=${input}]`).val("RED");
+            $(`input[name=${input}]`).css('background-color', 'red');
         }
     });
+
+
  });
